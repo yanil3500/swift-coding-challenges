7873ae70298928c186874078ebbe612a3a9a82a0
yanil3500 <yanil3500@gmail.com>
2017-07-28 18:22:10 -0700

    implements method that remove an element after a certain index

--- find-the-nth-node-from-the-last-node/find-the-nth-node-from-the last-node.playground/Contents.swift
+++ find-the-nth-node-from-the-last-node/find-the-nth-node-from-the last-node.playground/Contents.swift
@@ -18,6 +18,7 @@ extension Node: CustomStringConvertible {
 
 class SLL<Element>{
     fileprivate var head : Node<Element>?
+    fileprivate var length : Int = 0
 }
 
 extension SLL: CustomStringConvertible {
@@ -37,20 +38,49 @@ extension SLL {
     func prepend(newData data: Element){
         if head == nil {
             head = Node(withData: data)
+            length += 1
         } else {
-            var newNode : Node? = Node(withData: data)
+            let newNode : Node? = Node(withData: data)
             newNode?.next = head
             head = newNode
+            length += 1
         }
+    }
+    
+    func remove(afterIndex index: Int) -> Element? {
+        if head != nil {
+            if index == 0 {
+                guard let deletedVal : Element = head?.data else { return nil }
+                head = head?.next
+                length -= 1
+                return deletedVal
+            }
+            var prev : Node<Element>?
+            var curr : Node? = head
+            var count : Int = 0
+            while count < index && curr?.next != nil {
+                prev = curr
+                curr = curr?.next
+                count += 1
+            }
+            prev?.next = curr?.next
+            curr?.next = nil
+            guard let deletedVal : Element = curr?.data else { return nil }
+            length -= 1
+            return deletedVal
+        }
+        return nil
     }
 }
 
 
 var a_list = SLL<String>()
-for letter in "mo' money, mo' problems".components(separatedBy: " "){
+for letter in "mo' money, more' problems".components(separatedBy: " "){
     a_list.prepend(newData: letter)
 }
 
+print(a_list)
+print(a_list.remove(afterIndex: 1))
 print(a_list)
 
 
